bb5e1b425c2b601de19afadc73c4ad2e2b7801ec
Samet ULUTÜRK <sametcan@hotmail.com.tr>
2020-10-20 17:15:51 +0300

    İKİNCİ ACIKLAMA

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-echo "samet ulutürk";
+echo "Öğrenci No: 16008118045";
+echo "<br>";
+echo "İsim Soyisim: Samet ULUTÜRK";
+echo "<br>";
+echo "Bölüm: Bilgisayar Mühendisliği Bölümü";
+echo "Sınıf: 3. Sınıf";
